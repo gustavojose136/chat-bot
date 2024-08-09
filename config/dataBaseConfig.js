7b73f42eee5f0c4ad012e2e7b58ba2371c7e45e2
gutsz136 <gjose2980@gmail.com>
2024-08-09 20:29:47 -0300

    Add connection with mongo prod

--- config/dataBaseConfig.js
+++ config/dataBaseConfig.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
+//dev
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect('mongodb://0.0.0.0:27017/clinicaDB', {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log('MongoDB conectado com sucesso');
+//     } catch (error) {
+//         console.error('Erro ao conectar ao MongoDB', error);
+//         process.exit(1);
+//     }
+// };
+
+//prod
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://0.0.0.0:27017/clinicaDB', {
+        await mongoose.connect('mongodb+srv://grosa:teste123@chat-bot.doacpds.mongodb.net/', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
